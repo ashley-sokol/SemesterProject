cd25c55ca537c574dca192696339fd3c65f61e66
mqx4vk <143546242+mqx4vk@users.noreply.github.com>
2024-02-12 17:16:48 -0500

    initial creation of workplace app through django

---
+++ workplace_violation_project/workplace_violation_project/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for workplace_violation_project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workplace_violation_project.settings')
+
+application = get_asgi_application()
