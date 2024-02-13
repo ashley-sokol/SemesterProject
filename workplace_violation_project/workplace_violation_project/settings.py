7e18c176f2b06a23539ab184bee12872a2ae529d
mqx4vk <143546242+mqx4vk@users.noreply.github.com>
2024-02-12 19:27:55 -0500

    initial creation of index and login site

--- workplace_violation_project/workplace_violation_project/settings.py
+++ workplace_violation_project/workplace_violation_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'workplace_violation_app',
+    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
