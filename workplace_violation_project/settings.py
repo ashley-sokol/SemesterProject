fead400cd50a66d82e70b9b36fe30c1d72befba1
gavinvc <tzn4fx@virginia.edu>
2024-02-15 16:44:55 -0500

    fixed silly mistake in installed_apps

--- workplace_violation_project/settings.py
+++ workplace_violation_project/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'workplace_violation_app',
     'django_bootstrap5',
-    'bootstrap5'
+    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
