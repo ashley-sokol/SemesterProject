a16178f23e6db6995bf36cb6b3a15995d83578d3
mqx4vk <143546242+mqx4vk@users.noreply.github.com>
2024-02-20 13:49:49 -0500

    file structure refactor

--- workplace_violation_project/workplace_violation_project/settings.py
+++ workplace_violation_project/workplace_violation_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'workplace_violation_app',
-    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,14 +72,11 @@ WSGI_APPLICATION = 'workplace_violation_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'deqk7i3kso31tp',
-        'USER': 'blrpkujmskrqge',
-        'PASSWORD': '9e13376e2dd7352162e7443d5d089110dfaed010e993cf54e2d90d51a0709329',
-        'HOST': 'ec2-34-236-199-229.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -126,13 +121,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Activate Django-Heroku.
-# Use this code to avoid the psycopg2 / django-heroku error!
-# Do NOT import django-heroku above!
-try:
-    if 'HEROKU' in os.environ:
-        import django_heroku
-        django_heroku.settings(locals())
-except ImportError:
-    found = False
