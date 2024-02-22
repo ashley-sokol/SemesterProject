5f7b320afe812ed70795dd33214359146b8ce37c
gavinvc <tzn4fx@virginia.edu>
2024-02-22 11:50:01 -0500

    began working on model

--- workplace_violation_app/models.py
+++ workplace_violation_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    google_auth_token = models.CharField(max_length=100, default="")
+    is_admin = models.BooleanField(default=False)
+    username = models.CharField(max_length=50, blank=False, default="")
+
+    def __str__(self):
+        return self.username
+    
+    def is_authenticated(self):
+        return not self.google_auth_token == ""
