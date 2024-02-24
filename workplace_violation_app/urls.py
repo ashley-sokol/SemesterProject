2f93276fe3f4e9801067ea8ceea6b20e44af0ecb
Vineel <saitejan@gmail.com>
2024-02-24 16:29:40 -0500

    Added a button to redirect to a table of submissions.

--- workplace_violation_app/urls.py
+++ workplace_violation_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .views import LoginView, IndexView
+from .views import LoginView, IndexView, SubmissionsTableView
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
 
 app_name = 'workplace_violation_app'
 
 urlpatterns = [
+    path('submissions_table/', SubmissionsTableView.as_view(), name='submissions_table'),
     path('login/', LoginView.as_view(), name='login'),
     path('', IndexView.as_view(), name='index'),
     path('', TemplateView.as_view(template_name="login.html")),
