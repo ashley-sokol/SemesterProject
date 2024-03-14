cce64bd8b4b26b693378047cfe7e9974d4b1f7b5
anikal13 <anikaleela@gmail.com>
2024-03-14 13:37:52 -0400

    created two successful tests for CustomUser class

--- workplace_violation_app/tests.py
+++ workplace_violation_app/tests.py
@@ -1,18 +1,33 @@
 from django.test import TestCase
 from workplace_violation_app.models import AnonReportInfo
+from workplace_violation_app.models import CustomUser
 from django.utils import timezone
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.core.files import File
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.contrib.auth.models import AbstractUser, User
+
 
 # Create your tests here.
+
+class CustomUserTest(TestCase):
+    #test to ensure we can successfully make a custom user (CustomUser model)
+    def test_creating_user(self):
+        CustomUser.objects.create(username="hello")
+        self.assertEqual(len(CustomUser.objects.all()), 1)
+
+    #test to check is_admin field for CustomUser model
+    def test_check_admin_user(self):
+        user=CustomUser.objects.create(username="hello")
+        user.is_admin=True
+        self.assertTrue(user.is_admin)
 class AnonReportInfoTest(TestCase):
+    #practice test to ensure github actions is working. can delete later in the project
     def test_true(self):
         self.assertTrue(True)
+
+    #test below not working bc of some storage problem. trying to sort it out but not needed for now
     #def test_create_anon_report(self):
-        """
-        check if making an anon user works
-        """
        # date = timezone.now().date()
        # text = "hi"
         #file= SimpleUploadedFile(
