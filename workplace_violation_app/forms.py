45cdc75d464953ed280b5fc57eeab4d3f9e934c3
gavinvc <tzn4fx@virginia.edu>
2024-03-19 22:36:27 -0400

    Began UI changes and case number search

--- workplace_violation_app/forms.py
+++ workplace_violation_app/forms.py
@@ -2,16 +2,14 @@ from django import forms
 from .models import Report
 
 class AnonymousForm(forms.Form):
-     report_date = forms.DateField()
-     report_text = forms.CharField()
-     report_file = forms.FileField(required=False)
+     report_date = forms.DateField(widget=forms.DateInput(attrs={'placeholder': 'Enter date here.', 'class': 'form-control'}))
+     report_text = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Enter detailed report here.', 'style': 'height: 300px;', 'class': 'form-control'}))
+     report_file = forms.FileField(required=False, widget=forms.FileInput(attrs={'class': 'form-control-file'}))
 
 
      # def cleaned_data(self):
      #    cleaned_data = super().clean()
      #    file = cleaned_data.get("report_file")
-
+     
      #    if form.is_valid():
-
-
 
