bf3159f856c5d642b1c4a6c3f918b6df7a0a9a57
mqx4vk <143546242+mqx4vk@users.noreply.github.com>
2024-03-20 14:30:14 -0400

    Added common header to the base html, added submission deletion, added common user site.

--- workplace_violation_app/urls.py
+++ workplace_violation_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import LoginView, IndexView, SubmissionsTableView, logout_view
+from .views import *
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
 
@@ -7,11 +7,12 @@ app_name = 'workplace_violation_app'
 
 urlpatterns = [
     path('submissions_table/', SubmissionsTableView.as_view(), name='submissions_table'),
-    path('user_submissions/', SubmissionsTableView.as_view(), name='user_submissions'),
+    path('user_submissions/', UserSubmissionsTableView.as_view(), name='user_submissions'),
     path('login/', LoginView.as_view(), name='login'),
     path('', IndexView.as_view(), name='index'),
     path('', TemplateView.as_view(template_name="login.html")),
     path('accounts/', include('allauth.urls')),
     path('logout/', logout_view, name='logout'),
     path('submissions_table/serve_file/<str:file_path>/', SubmissionsTableView.as_view(), name='serve_file'),
+    path('delete_submission', DeleteSubmission.as_view(), name="delete_submission")
 ]
