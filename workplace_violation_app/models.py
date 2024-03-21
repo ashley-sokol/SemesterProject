8039f7bcb456e2a5407c18f98ccbb7d397f91fa2
anikal13 <anikaleela@gmail.com>
2024-03-21 13:57:00 -0400

    added report_number field to Report

--- workplace_violation_app/models.py
+++ workplace_violation_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 from django.contrib.auth.models import AbstractUser, User
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
@@ -20,6 +21,7 @@ class Report(models.Model): #previously anonreportinfo
     report_text = models.TextField(null=False, blank= False)
     report_file = models.FileField(storage=CustomS3Storage())    #Should save in Amazon s3 bucket
     report_status = models.CharField(max_length=100, default='New')  # status of report
+    report_number=models.UUIDField(primary_key=True, editable=False)
     is_seen = models.BooleanField(default=False)
 
     def __str__(self):
