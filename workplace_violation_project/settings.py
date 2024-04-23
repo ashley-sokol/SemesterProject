a5041e13d356bb2935e72f9905fcd4291f781b04
gavinvc <126908883+gavinvc@users.noreply.github.com>
2024-04-23 09:39:14 -0400

    commenting out local sqlite logic

--- workplace_violation_project/settings.py
+++ workplace_violation_project/settings.py
@@ -140,8 +140,8 @@ DATABASES = {
 
 if 'test' in sys.argv or 'test_coverage' in sys.argv: #Covers regular testing and django-coverage
     DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
-if not 'HEROKU' in os.environ: #if on local environment it will make local database and not use production database
-    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+#if not 'HEROKU' in os.environ: #if on local environment it will make local database and not use production database
+#    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
